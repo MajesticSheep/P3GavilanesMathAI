21c6e9f16c9038ad09b44361c6cb4b8c36a0bf7f
Giannamia Gavilanes <719613@student.dusd.net>
2026-02-28 21:51:42 -0800

    Worked on assignment
    I worked on the game programming math assignment for the 2D portion.

---
+++ GavilanesMathAIP3/Assets/Drive.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// A very simplistic car driving on the x-z plane.
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public GameObject fuel;
+
+    void Start()
+    {
+
+    }
+
+    void CalculateAngle()
+    {
+        Vector3 tankForward = transform.up;
+        Vector3 fuelDirection = fuel.transform.position - transform.position;
+
+        Debug.DrawRay(this.transform.position, tankForward, Color.green, 2);
+        Debug.DrawRay(this.transform.position, fuelDirection, Color.red, 2);
+    }
+
+    void CalculateDistance()
+    {
+        float distance = Mathf.Sqrt(Mathf.Pow(fuel.transform.position.x - transform.position.x, 2) + Mathf.Pow(fuel.transform.position.z - transform.position.z, 2));
+
+        Vector3 fuelPos = new Vector3(fuel.transform.position.x, 0, fuel.transform.position.z);
+        Vector3 tankPos = new Vector3(transform.position.x, 0, transform.position.z);
+        float uDistance = Vector3.Distance(fuelPos, tankPos);
+
+        Vector3 tankToFuel = fuelPos - tankPos;
+
+
+        Debug.Log("Distance: " + distance);
+        Debug.Log("U Distance: " + uDistance);
+        Debug.Log("V Magnitude: " + tankToFuel.magnitude);
+        Debug.Log("V sqrMagnitude: " + tankToFuel.sqrMagnitude);
+    }
+
+    void LateUpdate()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, translation, 0);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, 0, -rotation);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CalculateDistance();
+            CalculateAngle();
+        }
+
+    }
+}
