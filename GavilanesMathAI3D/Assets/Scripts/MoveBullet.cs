38acf88c02b04c29094c7989e918b59e46ef3076
Giannamia Gavilanes <719613@student.dusd.net>
2026-03-12 11:45:19 -0700

    Worked on Physics AI
    I worked on half of the Physics of AI assignment.

---
+++ GavilanesMathAI3D/Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
+        
+    }
+}
---
+++ GavilanesMathAI3D/Assets/Scripts/Script/SecondsUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStartOffset = 0;
+    bool gotStartTime = false;
+    public float speed = 1f;
+
+
+    void Update()
+    {
+        if(!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, (Time.realtimeSinceStartup - timeStartOffset) * speed);
+    }
+}
