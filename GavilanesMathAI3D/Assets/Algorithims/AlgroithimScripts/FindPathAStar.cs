8ad694632a94a063ac95a3100e4be3ba827e59a7
Giannamia Gavilanes <719613@student.dusd.net>
2026-03-18 11:47:35 -0700

    Worked
    I worked on the assignment.

---
+++ GavilanesMathAI3D/Assets/Algorithims/AlgroithimScripts/FindPathAStar.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMarker
+{
+    public MapLocation location;
+    public float G;
+    public float H;
+    public float F;
+    public GameObject marker;
+    public PathMarker parent;
+
+    public PathMarker(MapLocation l, float g, float h, float f, GameObject marker, PathMarker p)
+    {
+        location = l;
+        G = g;
+        H = h;
+        F = f;
+        this.marker = marker;
+        parent = p;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            return location.Equals(((PathMarker) obj).location);
+        }
+    }
+}
+
+
+public class FindPathAStar : MonoBehaviour
+{
+    public Maze maze;
+    public Material closedMaterial;
+    public Material openMaterial;
+
+    List<PathMarker> open = new List<PathMarker>();
+    List<PathMarker> closed = new List<PathMarker>();
+
+    public GameObject start;
+    public GameObject end;
+    public GameObject pathP;
+
+    PathMarker goalNode;
+    PathMarker startNode;
+
+    PathMarker lastPos;
+    bool done = false;
+
+
+    void RemoveAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+        foreach (GameObject m in markers)
+        {
+            Destroy(m);
+        }
+    }
+
+    void BeginSearch()
+    {
+        done = false;
+        RemoveAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+        for (int z = 1; z < maze.depth - 1; z++)
+            for (int x = 1; x < maze.width -1; x++)
+            {
+                if (maze.map[x, z] != 1)
+                {
+                    locations.Add(new MapLocation(x, z));
+                }
+            }
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0, Instantiate(start, startLocation, Quaternion.identity), null);
+
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+        goalNode = new PathMarker(new MapLocation(locations[1].x, locations[1].z), 0, 0, 0, Instantiate(end, goalLocation, Quaternion.identity), null);
+
+        open.Clear();
+        closed.Clear();
+        open.Add(startNode);
+        lastPos = startNode;
+    }
+
+    void Search(PathMarker thisNode)
+    {
+        if(thisNode.Equals(goalNode))
+        {
+            done = true;
+            return;
+            //goal has been found
+        }
+        
+        foreach(MapLocation dir in maze.directions)
+        {
+            MapLocation neighbor = dir + thisNode.location;
+            if (maze.map[neighbor.x, neighbor.z] == 1) continue;
+            if(neighbor.x < 1 || neighbor.x >= maze.width || neighbor.z < 1 || neighbor.z >= maze.depth) continue;
+            if (IsClosed(neighbor)) continue;
+
+            float G = Vector2.Distance(thisNode.location.ToVector(), neighbor.ToVector()) + thisNode.G;
+            float H = Vector2.Distance(neighbor.ToVector(), goalNode.location.ToVector());
+            float F = G + H;
+        }
+
+
+        bool IsClosed(MapLocation marker)
+        {
+            foreach (PathMarker p in closed)
+            {
+                if(p.location.Equals(marker)) return true;
+            }
+            return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            BeginSearch();
+        }
+    }
+}
